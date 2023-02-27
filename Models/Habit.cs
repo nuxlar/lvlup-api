a973da982d9eb0afa8d1d7c46eecb0b7b1a3fba5
Nux <romy.mag9@gmail.com>
2023-02-27 15:09:43 -0500

    relationships

--- Models/Habit.cs
+++ Models/Habit.cs
@@ -8,6 +8,8 @@ public class Habit
   public int Multiplier { get; set; }
   public DateOnly Date { get; set; }
   public Boolean Completed { get; set; }
+  public int UserId { get; set; }
+  public User User { get; set; }
 
   public Habit()
   {
--- Models/User.cs
+++ Models/User.cs
@@ -6,10 +6,15 @@ public class User
   public string? Name { get; set; }
   public int Xp { get; set; }
   public int XpToLevelUp { get; set; }
+  // Relationships One -> Has Many
+  public List<Habit> Habits { get; set; }
+  public List<HasDone> HasDones { get; set; }
 
   public User()
   {
     Xp = 0;
     XpToLevelUp = 100;
+    Habits = new List<Habit> { };
+    HasDones = new List<HasDone> { };
   }
 }
